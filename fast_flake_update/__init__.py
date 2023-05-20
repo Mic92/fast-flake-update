cff6848932916fbe91d583a0ea920590eab322ad
Jörg Thalheim <joerg@thalheim.io>
2023-05-20 18:12:18 +0200

    first commit

---
+++ fast_flake_update/__init__.py
@@ -0,0 +1,53 @@
+#!/usr/bin/env python3
+
+import json
+import subprocess
+import sys
+from pathlib import Path
+from tempfile import TemporaryDirectory
+
+
+def main() -> None:
+    if len(sys.argv) != 3:
+        print(f"Usage: {sys.argv[0]} <inputname> <repo>")
+        sys.exit(1)
+    inputname = sys.argv[1]
+    local_checkout = Path(sys.argv[2])
+
+    flake_lock = Path("flake.lock")
+    if not flake_lock.exists():
+        print("flake.lock not found")
+        sys.exit(1)
+    lock = json.load(flake_lock.open())
+    flake_input = lock["nodes"].get(inputname)
+    if not flake_input:
+        print(f"input {inputname} not found in flake.lock")
+        print(f"available inputs: {lock['nodes'].keys()}")
+        sys.exit(1)
+    with TemporaryDirectory() as tmpdir:
+        archive = Path(tmpdir) / "latest.tar.gz"
+        res = subprocess.run(
+            ["git", "-C", str(local_checkout), "rev-parse", "HEAD"],
+            check=True,
+            text=True,
+            stdout=subprocess.PIPE,
+        )
+        rev = res.stdout.strip()
+        subprocess.run(["git", "-C", str(local_checkout), "archive", "-o", archive, rev])
+        res = subprocess.run(
+            ["nix-prefetch-url", "--unpack", f"file://{archive}", "--name", "source"],
+            stdout=subprocess.PIPE,
+            text=True,
+            check=True,
+        )
+        source_hash = res.stdout.strip()
+        res = subprocess.run(["nix", "hash", "to-sri", "--type", "sha256", source_hash], check=True, stdout=subprocess.PIPE, text=True)
+        flake_input["locked"]["narHash"] = res.stdout.strip()
+        flake_input["locked"]["rev"] = rev
+    tmp = flake_lock.with_name("flake.lock.tmp")
+    tmp.write_text(json.dumps(lock, indent=2, sort_keys=True) + "\n")
+    tmp.rename(flake_lock)
+
+
+if __name__ == "__main__":
+    main()
---
+++ flake.nix
@@ -0,0 +1,25 @@
+{
+  description = "Update nix flake git/github inputs from local git repositories checkouts";
+
+  inputs = {
+    nixpkgs.url = "github:NixOS/nixpkgs/nixos-unstable";
+    flake-parts.url = "github:hercules-ci/flake-parts";
+    flake-parts.inputs.nixpkgs-lib.follows = "nixpkgs";
+    treefmt-nix.url = "github:numtide/treefmt-nix";
+    treefmt-nix.inputs.nixpkgs.follows = "nixpkgs";
+  };
+
+  outputs = inputs@{ flake-parts, ... }:
+    flake-parts.lib.mkFlake { inherit inputs; } ({ lib, ... }: {
+      systems = lib.systems.flakeExposed;
+      perSystem = { pkgs, ... }: {
+        #imports = [ ./treefmt.nix ];
+        packages.default = pkgs.python3.pkgs.buildPythonPackage {
+          pname = "fast-flake-update";
+          version = "0.1.0";
+          src = ./.;
+          doCheck = false;
+        };
+      };
+    });
+}
