b59404f81b37ce13506e0c2efd48aa3cb8f89eed
Jörg Thalheim <joerg@thalheim.io>
2024-09-30 08:34:18 +0200

    update formatter

--- treefmt.nix
+++ treefmt.nix
@@ -6,6 +6,14 @@
       projectRootFile = "flake.lock";
 
       programs.prettier.enable = true;
+
+      programs.nixfmt.enable = true;
+      programs.nixfmt.package = pkgs.nixfmt-rfc-style;
+
+      programs.deadnix.enable = true;
+      programs.ruff.check = true;
+      programs.ruff.format = true;
+
       programs.mdsh.enable = true;
       programs.mdsh.package = pkgs.runCommand "mdsh"
         {
@@ -15,39 +23,6 @@
         makeWrapper ${pkgs.lib.getExe pkgs.mdsh} $out/bin/mdsh \
           --set PATH ${pkgs.lib.makeBinPath [ pkgs.bash pkgs.python3 ]}
       '';
-
-      settings.formatter = {
-        prettier.options = [ "--prose-wrap" "always" ];
-
-        nix = {
-          command = "sh";
-          options = [
-            "-eucx"
-            ''
-              # First deadnix
-              ${pkgs.lib.getExe pkgs.deadnix} --edit "$@"
-              # Then nixpkgs-fmt
-              ${pkgs.lib.getExe pkgs.nixpkgs-fmt} "$@"
-            ''
-            "--"
-          ];
-          includes = [ "*.nix" ];
-          excludes = [ "nix/sources.nix" ];
-        };
-
-        python = {
-          command = "sh";
-          options = [
-            "-eucx"
-            ''
-              ${lib.getExe pkgs.ruff} --fix "$@"
-              ${lib.getExe pkgs.ruff} format "$@"
-            ''
-            "--" # this argument is ignored by bash
-          ];
-          includes = [ "*.py" ];
-        };
-      };
     };
   };
 }
