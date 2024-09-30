23d7903e724d716b43dcb33bc3467abdf39dbc07
Jörg Thalheim <joerg@thalheim.io>
2024-09-30 08:35:00 +0200

    reformat

--- flake.nix
+++ flake.nix
@@ -9,7 +9,8 @@
     treefmt-nix.inputs.nixpkgs.follows = "nixpkgs";
   };
 
-  outputs = inputs:
+  outputs =
+    inputs:
     inputs.flake-parts.lib.mkFlake { inherit inputs; } {
       systems = [
         "aarch64-linux"
@@ -20,25 +21,36 @@
         "x86_64-darwin"
       ];
       imports = [ ./treefmt.nix ];
-      perSystem = { config, self', pkgs, lib, ... }: {
-        packages.fast-flake-update = pkgs.python3.pkgs.buildPythonPackage {
-          pname = "fast-flake-update";
-          version = "0.1.0";
-          src = ./.;
-          makeWrapperArgs = [
-            "--prefix PATH : ${pkgs.lib.makeBinPath [
-              pkgs.git pkgs.nixVersions.nix_2_19
-            ]}"
-          ];
-          doCheck = false;
+      perSystem =
+        {
+          config,
+          self',
+          pkgs,
+          lib,
+          ...
+        }:
+        {
+          packages.fast-flake-update = pkgs.python3.pkgs.buildPythonPackage {
+            pname = "fast-flake-update";
+            version = "0.1.0";
+            src = ./.;
+            makeWrapperArgs = [
+              "--prefix PATH : ${
+                pkgs.lib.makeBinPath [
+                  pkgs.git
+                  pkgs.nixVersions.nix_2_19
+                ]
+              }"
+            ];
+            doCheck = false;
+          };
+          packages.default = config.packages.fast-flake-update;
+          checks =
+            let
+              packages = lib.mapAttrs' (n: lib.nameValuePair "package-${n}") self'.packages;
+              devShells = lib.mapAttrs' (n: lib.nameValuePair "devShell-${n}") self'.devShells;
+            in
+            packages // devShells;
         };
-        packages.default = config.packages.fast-flake-update;
-        checks =
-          let
-            packages = lib.mapAttrs' (n: lib.nameValuePair "package-${n}") self'.packages;
-            devShells = lib.mapAttrs' (n: lib.nameValuePair "devShell-${n}") self'.devShells;
-          in
-          packages // devShells;
-      };
     };
 }
--- treefmt.nix
+++ treefmt.nix
@@ -1,28 +1,38 @@
-{ lib, inputs, ... }: {
+{ lib, inputs, ... }:
+{
   imports = [ inputs.treefmt-nix.flakeModule ];
-  perSystem = { pkgs, ... }: {
-    treefmt = {
-      # Used to find the project root
-      projectRootFile = "flake.lock";
+  perSystem =
+    { pkgs, ... }:
+    {
+      treefmt = {
+        # Used to find the project root
+        projectRootFile = "flake.lock";
 
-      programs.prettier.enable = true;
+        programs.prettier.enable = true;
 
-      programs.nixfmt.enable = true;
-      programs.nixfmt.package = pkgs.nixfmt-rfc-style;
+        programs.nixfmt.enable = true;
+        programs.nixfmt.package = pkgs.nixfmt-rfc-style;
 
-      programs.deadnix.enable = true;
-      programs.ruff.check = true;
-      programs.ruff.format = true;
+        programs.deadnix.enable = true;
+        programs.ruff.check = true;
+        programs.ruff.format = true;
 
-      programs.mdsh.enable = true;
-      programs.mdsh.package = pkgs.runCommand "mdsh"
-        {
-          nativeBuildInputs = [ pkgs.makeWrapper ];
-        } ''
-        mkdir -p $out/bin
-        makeWrapper ${pkgs.lib.getExe pkgs.mdsh} $out/bin/mdsh \
-          --set PATH ${pkgs.lib.makeBinPath [ pkgs.bash pkgs.python3 ]}
-      '';
+        programs.mdsh.enable = true;
+        programs.mdsh.package =
+          pkgs.runCommand "mdsh"
+            {
+              nativeBuildInputs = [ pkgs.makeWrapper ];
+            }
+            ''
+              mkdir -p $out/bin
+              makeWrapper ${pkgs.lib.getExe pkgs.mdsh} $out/bin/mdsh \
+                --set PATH ${
+                  pkgs.lib.makeBinPath [
+                    pkgs.bash
+                    pkgs.python3
+                  ]
+                }
+            '';
+      };
     };
-  };
 }
